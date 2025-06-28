7e34a9292b5a0161589e93c4d90d7b905ff75ca0
bhuvan649 <vasulebhuvanesh@gmail.com>
2025-06-28 15:30:36 +0530

    updated LoginPopup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import Placeorder from './pages/PlaceOrder/PlaceOrder';
@@ -6,11 +6,16 @@ import Cart from './pages/Cart/Cart';
 import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
 import './App.css'
+import LoginPopup from './components/LoginPopup/LoginPopup';
 const App = () => {
+
+const [showLogin,setShowLogin]=useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
         <div className="app">
-      <Navbar />
+      <Navbar setShowLogin={setShowLogin} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
